fb6fd83704a1892e32cb3e42e4f7f322635a4542
varunbajaj02 <varunbajaj3690@gmail.com>
2025-04-09 15:32:05 +0530

    addition

---
+++ addition.java
@@ -0,0 +1,10 @@
+package new_java;
+
+public class addition {
+    public static void main(String[] args){
+        int a=10,b=5;
+        int sum=a+b;
+        System.out.println("add: " +sum);
+    }
+}
+

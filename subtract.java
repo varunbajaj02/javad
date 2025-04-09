54afdc63c6fc0868a128094ecc2a2cf762185eb5
varunbajaj02 <varunbajaj3690@gmail.com>
2025-04-09 15:34:45 +0530

    subtract

---
+++ subtract.java
@@ -0,0 +1,9 @@
+package new_java;
+
+public class subtract {
+    public static void main(String[] args){
+        int a=10,b=5;
+        int sub=a-b;
+        System.out.println("sub: " +sub);
+    }
+}
